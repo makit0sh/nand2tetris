f2f2b762b9d8e377a0ff52fca13ef4d646a4d029
Toshiya Maki(MBA) <t.maki.0402@gmail.com>
2017-09-05 00:08:51 +0900

    implemented SymbolTable

--- 06/SymbolTable.cpp
+++ 06/SymbolTable.cpp
@@ -1,16 +1,51 @@
 #include "SymbolTable.h"
 
-
-void SymbolTable::addEntry(std::string symbol, int address)
+SymbolTable::SymbolTable()
 {
+    stable["SP"] = 0;
+    stable["LCL"] = 1;
+    stable["ARG"] = 2;
+    stable["THIS"] = 3;
+    stable["THAT"] = 4;
+    stable["R0"] = 0;
+    stable["R1"] = 1;
+    stable["R2"] = 2;
+    stable["R3"] = 3;
+    stable["R4"] = 4;
+    stable["R5"] = 5;
+    stable["R6"] = 6;
+    stable["R7"] = 7;
+    stable["R8"] = 8;
+    stable["R9"] = 9;
+    stable["R10"] = 10;
+    stable["R11"] = 11;
+    stable["R12"] = 12;
+    stable["R13"] = 13;
+    stable["R14"] = 14;
+    stable["R15"] = 15;
+    stable["SCREEN"] = 16384;
+    stable["KBD"] = 24576;
+}
 
+void SymbolTable::addEntry(std::string symbol, int address) throw(std::runtime_error)
+{
+    if (contains(symbol)) {
+        throw std::runtime_error("redundunt symbol found");
+    }else{
+        stable[symbol] = address;
+    }
 }
 
 bool SymbolTable::contains(std::string symbol)
 {
-
+    return stable.find(symbol) != stable.end();
 }
 
-int SymbolTable::getAddress(std::string symbol)
+int SymbolTable::getAddress(std::string symbol) throw(std::runtime_error)
 {
+    if (contains(symbol)){
+        return stable[symbol];
+    }else{
+        throw std::runtime_error("cannnot resolve symbol");
+    }
 }
--- 06/SymbolTable.h
+++ 06/SymbolTable.h
@@ -3,14 +3,17 @@
 
 #include <iostream>
 #include <string>
+#include <map>
 
 class SymbolTable {
+    private:
+        std::map<std::string, int> stable;
     public:
         SymbolTable();
 
-        void addEntry(std::string symbol, int address);
+        void addEntry(std::string symbol, int address) throw (std::runtime_error);
         bool contains(std::string symbol);
-        int getAddress(std::string symbol);
+        int getAddress(std::string symbol) throw (std::runtime_error);
 };
 
 #endif // SYMBOLTABLE_H_
